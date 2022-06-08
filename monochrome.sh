80f7f60d38596d6f9d61cd78dd1854c1a02883ca
hobbitpl <minimalnie@gmail.com>
2022-06-08 23:31:32 +0200

    Update monochrome.sh

--- monochrome.sh
+++ monochrome.sh
@@ -2,5 +2,5 @@
 
 for f in *.tif; do
 	echo "Conversion of file"."$f"." has started"
-	time convert -colors 2 -density 600x600 -colorspace gray -compress group4 ./"$f" ./output_bitonal/"x_${f%}"
+	time convert -colors 2 -density 600x600 -colorspace gray -compress group4 ./"$f" ./output_bitonal/"bitonal_${f%}"
 done
